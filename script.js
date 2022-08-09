bc17a1db501aee0655e48c28d88b8f257867e987
Kevin Quinlan <quinlan72@aol.com>
2022-08-09 18:43:30 -0500

    added starter code and a few var

---
+++ script.js
@@ -0,0 +1,32 @@
+const quizDiv = document.getElementById("Start")
+const submitButton = document.getElementById("submit")
+const resultsDiv = document.getElementById("results")
+const questions = [
+    {
+        question: "JavaScript is a ___ -side programming language.",
+        answers: {
+            a: "client", 
+            b: "server",
+            c: "both",
+            d: "none"
+        },
+        correctAnswer: "c"
+    }, {
+        question: "Which are the correct “if” statements to execute certain code if “x” is equal to 2?",
+        answers: {
+            a: "if(x2)",
+            b: "if(x = 2",
+            c: "if(x == 2)",
+            d: "if(x != 2)"
+        },
+        correstAnswer: "c"
+    }, {
+        question: "Who invented JavaScript?",
+        answers: {
+            a: "Douglas Crockford",
+            b: "Sheryl Sandberg",
+            c: "Brendan Eich"
+        },
+        correctAnswer: "c"
+    }
+];
